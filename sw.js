af0e3a3ca3b891c8ba13fa4cf16c73310c935510
Chenx Dayat <dayatbwi@gmail.com>
2026-08-06 12:37:12 +0700

    Delete sw.js
    Signed-off-by: Chenx Dayat <dayatbwi@gmail.com>

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('message', function(event) {
-    if (event.data.action === 'clearCache') {
-        caches.keys().then(function(cacheNames) {
-            cacheNames.forEach(function(cacheName) {
-                caches.delete(cacheName);
-            });
-        });
-    }
-});
